4acc35bb01f4789c121125e45d654c22b7a48b05
MuhammadZariyanAsif123 <33691797+MuhammadZariyanAsif123@users.noreply.github.com>
2022-11-27 15:48:15 +0500

    2 files modified

--- src/App.js
+++ src/App.js
@@ -9,11 +9,20 @@ import { TextForm } from './TextForm';
 export default function App() {
   return (
     <>
-      <NavBar />
-      <h2>Enter the text to analyze below </h2>
-      <br></br>
-      <TextForm />
-      <AboutUs />
+      <Router>
+        <NavBar />
+        <h2>Enter the text to analyze below </h2>
+        <br></br>
+        <TextForm />
+        <AboutUs />
+        <div className="container my-3">
+          <Switch>
+            <Route path="./AboutUs">
+              <AboutUs />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
       <button type="button" className="btn btn-primary">
         Convert to Upper Case
       </button>
--- src/NavBar.js
+++ src/NavBar.js
@@ -2,42 +2,44 @@ import React from 'react';
 import Link from 'react-router-dom';
 
 export function NavBar() {
-  return (
+  return
+  (
+
     <nav className="navbar navbar-expand-lg bg-light">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-          MY APPLICATION
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
-            </li>
-            <li className="nav-item"></li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                About Us
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link ">Disabled</a>
-            </li>
-          </ul>
-        </div>
+    <div className="container-fluid">
+      <a className="navbar-brand" href="#">
+        MY APPLICATION
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link className="nav-link active" aria-current="page" to="/">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item"></li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/AboutUs">
+              About Us
+            </Link>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link ">Disabled</a>
+          </li>
+        </ul>
       </div>
-    </nav>
-  );
+    </div>
+  </nav>
+  )
 }
