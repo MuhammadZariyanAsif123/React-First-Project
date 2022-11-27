e7609ab3ca8d85ef6c15029e958a052f9d24d21a
MuhammadZariyanAsif123 <33691797+MuhammadZariyanAsif123@users.noreply.github.com>
2022-11-27 15:07:30 +0500

    2 files modified, 3 added

--- src/App.js
+++ src/App.js
@@ -1,57 +1,19 @@
 import React, { usestate } from 'react';
 import './style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { AboutUs } from './AboutUs';
+import { NavBar } from './NavBar';
+import { TextForm } from './TextForm';
 
-export default function App() 
-{
-  const [text, upperCase] = usestate('Enter Text here');
-  upperCase('ABCD');
-
+export default function App() {
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#"></a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Features
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Pricing
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled">Disabled</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <NavBar />
       <h2>Enter the text to analyze below </h2>
       <br></br>
-      <div>
-        <textarea className="form-control" value={text} rows="8"></textarea>
-      </div>
+      <TextForm />
+      <AboutUs />
       <button type="button" className="btn btn-primary">
         Convert to Upper Case
       </button>
---
+++ src/TextForm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function TextForm() {
+  return (
+    <div>
+      <textarea className="form-control" rows="8"></textarea>
+    </div>
+  );
+}
